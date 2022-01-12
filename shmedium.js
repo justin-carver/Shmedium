df3e65594ac283a547ff9296c43dd285a0d29bec
Justin Carver <justin.carver@hotmail.com>
2022-01-12 01:53:08 -0600

    Function looks for medium hosted links, checks their paywall, and appends a tag to it.

---
+++ shmedium.js
@@ -0,0 +1,34 @@
+const getMediumArticles = async (url) => {
+    await fetch(`${url.href}`).then(function (response) {
+        return response.text();
+    }).then(function (data) {
+        if (!url.href.includes('/tag/')) {
+            if (data.includes('meteredContent')) {
+                let paywall = document.createElement('div');
+                paywall.classList.add('paywall-notif');
+                paywall.innerHTML = 'PAYWALL';
+                paywall.setAttribute('style', 
+                    `display: inline-flex; width: auto; padding: 0.25rem; 
+                    margin-left: 0.80rem; color: #000; background-color: #ffc017; 
+                    border-radius: 5px;`
+                );
+                url.parentElement.append(paywall);
+                url.parentElement.style.width = '120%';
+            }
+        }
+    }).catch(function (err) {
+        // There was an error
+        console.warn('Something went wrong.', err);
+    });
+}
+
+const init = async () => {
+    let urls = document.querySelectorAll('a');
+    for (let i = 0; i < urls.length; i++) {
+        if (urls[i].host === 'medium.com') {
+            getMediumArticles(urls[i]);
+        }
+    }
+}
+
+init();
